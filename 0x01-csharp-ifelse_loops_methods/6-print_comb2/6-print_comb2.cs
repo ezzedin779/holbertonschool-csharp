8099a82eecbbdf52df902960d89f0d9e9efc5cb8
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-15 18:28:55 +0200

    Not the same ?

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 89; i++)
+            {
+                if ((i % 10) > (i / 10))
+                {
+                    Console.Write("{0:d2}, ", i);
+                }
+            }
+            Console.WriteLine("89");
+        }
+    }
+}
