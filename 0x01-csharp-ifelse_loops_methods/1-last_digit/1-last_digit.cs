adfaf4ceb77c6b74809b659e15fb8f4877395cde
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-15 17:56:38 +0200

    The last digit is ?

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        Console.Write("The last digit of {0} is {1}", number, number % 10);
+        if (number % 10 == 0)
+        {
+            Console.WriteLine(" and is 0");
+        }
+        else if (number % 10 > 5)
+        {
+            Console.WriteLine(" and is greater than 5");
+        }
+        else if (number % 10 < 6)
+        {
+            Console.WriteLine(" and is less than 6 and not 0");
+        }
+    }
+}
