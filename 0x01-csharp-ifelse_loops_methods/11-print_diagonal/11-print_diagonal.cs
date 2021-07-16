2e08aaf095b67caf432eb46393adcec69ab45a2b
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-16 08:51:34 +0200

    Printing in diagonal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11_print_diagonal
+{
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            if (length <= 0)
+            {
+                Console.WriteLine();
+            }
+            for (int i = 0; i < length; i++)
+            {
+                string space = new String(' ', i);
+                Console.Write(space);
+                Console.WriteLine("\\");
+            }
+        }
+    }
+}
