718e7a07efad0fdda626e948261392b740e6c484
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-16 07:58:36 +0200

    Last Digit

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _8_print_last_digit
+{
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            int i = number;
+            if (number < 0)
+            {
+                i = i * -1;
+            }
+            Console.Write("{0}", i % 10);
+        }
+    }
+}
