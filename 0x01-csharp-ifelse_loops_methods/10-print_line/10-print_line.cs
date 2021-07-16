2a60d23d88ea9b4b20edd85d26d529382e970537
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-16 08:05:19 +0200

    Print a line ?

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10_print_line
+{
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+            if (length <= 0)
+            {
+                Console.WriteLine("");
+            }
+            for(int i = 1; i < length; i++)
+            {
+                Console.Write("_");
+            }
+            Console.WriteLine("_");
+        }
+    }
+}
