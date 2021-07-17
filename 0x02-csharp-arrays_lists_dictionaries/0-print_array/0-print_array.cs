68cd926528616672a63c983d683b770bcd95147f
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-17 17:12:03 +0200

    Create and Print and array ?

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,34 +1,31 @@
 ﻿using System;
 
-namespace _0_print_array
+class Array
 {
-    class Array
+    public static int[] CreatePrint(int size)
     {
-        public static int[] CreatePrint(int size)
+        if (size == 0)
         {
-            if (size == 0)
-            {
-                Console.WriteLine();
-            }
-            if (size < 0)
-            {
-                Console.WriteLine("Size cannot be negative");
-                return (null);
-            }
-            int[] tab = new int[size];
-            for (int i = 0; i < size; i++)
-            {
-                tab[i] = i;
-                if (i == size - 1)
-                {
-                    Console.WriteLine("{}", tab[i]);
-                }
-                else
-                {
-                    Console.Write("{} ", tab[i]);
-                }
-            }
-            return (tab);
+            Console.WriteLine();
         }
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        int[] tab = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            tab[i] = i;
+            if (i == size - 1)
+            {
+                Console.WriteLine(tab[i]);
+            }
+            else
+            {
+                Console.Write(tab[i] + " ");
+            }
+        }
+        return (tab);
     }
 }
