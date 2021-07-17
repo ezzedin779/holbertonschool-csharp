6602eb56df0eee60d9c423639d78b8b5fdf0d2df
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-17 16:13:41 +0200

    Create and print an array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _0_print_array
+{
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size == 0)
+            {
+                Console.WriteLine();
+            }
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return (null);
+            }
+            int[] tab = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                tab[i] = i;
+                if (i == size - 1)
+                {
+                    Console.WriteLine("{}", tab[i]);
+                }
+                else
+                {
+                    Console.Write("{} ", tab[i]);
+                }
+            }
+            
+        }
+    }
+}
