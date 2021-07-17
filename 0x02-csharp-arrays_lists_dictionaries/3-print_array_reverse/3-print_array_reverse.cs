910662e86ea00b427334b7ec75fba3ff1e70f4a3
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-17 17:48:31 +0200

    Reversed array ?

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length <= 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+        int i = 0;
+        for (i = array.Length - 1; i > 0; i--)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine(array[i]);
+    }
+}
+
