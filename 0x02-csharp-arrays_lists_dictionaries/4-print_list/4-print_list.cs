0f61773fd4d9357ac0c35ced98a4067a793a9a58
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-17 18:18:44 +0200

    Create & Print List ?

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        else if (size == 0)
+        {
+            Console.WriteLine();
+        }
+        int i = 0;
+        List<int> Ls = new List<int>();
+        for (i = 0; i < size; i++)
+        { 
+            Ls.Add(i);
+            if (i == size - 1)
+            {
+                Console.WriteLine(Ls[i]);
+            }
+            else
+            {
+                Console.Write(Ls[i] + " ");
+            }
+        }
+        return (Ls);
+    }
+}
