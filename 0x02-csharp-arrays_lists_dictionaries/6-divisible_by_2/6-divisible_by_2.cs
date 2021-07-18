df2fe00b712cd96d91b0e3be34f9653311fc4631
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-18 09:59:06 +0200

    Div by 2 ?

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> Ls = new List<bool>();
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (myList[i] % 2 == 0)
+                Ls.Add(true);
+            else
+                Ls.Add(false);
+        }
+        return (Ls);
+    }
+}
