040ec27f11ea8e1564b55012759ec93285d43c8f
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-18 23:50:42 +0200

    Playing with Stack ?

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class  MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int count = 0;
+        int count2 = 0;
+        int index = 0;
+        bool s = false;
+        bool check = false;
+        string first = "";
+        foreach (var i in aStack)
+        {
+            count += 1;
+            if (check == false)
+            {
+                first = i;
+                check = true;
+            }
+            if (i == search)
+            {
+                s = true;
+                index = count;
+            }
+        }
+        if (s == true)
+        {
+            for (count2 = 0; count2 < index; count2++)
+                aStack.Pop();
+        }
+        aStack.Push(newItem);
+        Console.WriteLine("Number of items: {0}", count);
+        if (count > 0)
+            Console.WriteLine("Top item: {0}", first);
+        else
+            Console.WriteLine("Stack is empty");
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, s);
+        return(aStack);
+    }
+}
