31724867149a717d0a241e9583bdaaa2de24249d
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-19 00:35:26 +0200

    Value of that node please ?

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class  LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int count = 0;
+        foreach (var i in myLList)
+        {
+            if (count == n)
+                return (i);
+            count++;
+        }
+        return (0);
+    }
+}
