ad382f6befe38e136dd761054c0f6dbcac60134a
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-18 10:34:22 +0200

    Delete at index ?

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index > myList.Count - 1 || index < 0)
+            return (myList);
+        myList.Remove(myList[index]);
+        return (myList);
+    }
+}
