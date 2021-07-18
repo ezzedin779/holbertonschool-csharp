7df574e36199564ec29c57e308c5c1933542a709
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-18 23:14:37 +0200

    Add Unique Items

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class  List
+{
+    public static int Sum(List<int> myList)
+    {
+        List<int> Ls = new List<int>();
+        foreach (var i in myList)
+        {
+            if (Ls.Contains(i))
+                continue;
+            Ls.Add(i);
+        }
+        int result = 0;
+        for (int i = 0; i < Ls.Count; i++)
+            result = result + Ls[i];
+        return (result);
+    }
+}
