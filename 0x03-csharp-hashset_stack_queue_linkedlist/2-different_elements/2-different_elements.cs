91cd5c7c4646b59fbe6dc33641ca2c9ef75dfbcc
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-18 23:25:07 +0200

    Different items ?

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class  List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> Ls = new List<int>();
+        foreach (var i in list1)
+        {
+            if (list2.Contains(i))
+                continue;
+            Ls.Add(i);
+        }
+        foreach (var i in list2)
+        {
+            if (list1.Contains(i))
+                continue;
+            Ls.Add(i);
+        }
+        Ls.Sort();
+        return (Ls);
+    }
+}
