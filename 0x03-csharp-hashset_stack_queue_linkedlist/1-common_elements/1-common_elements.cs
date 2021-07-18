d3896346f3f92e6f5cbc951de01a8f4b820abd9b
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-18 23:20:17 +0200

    Common list ?

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class  List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> Ls = new List<int>();
+        foreach (var i in list1)
+        {
+            if (list2.Contains(i))
+                Ls.Add(i);
+        }
+        Ls.Sort();
+        return (Ls);
+    }
+}
