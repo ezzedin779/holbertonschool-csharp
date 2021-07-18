63fb483d97e58a9cd1ae35184fded66a4b54ea7b
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-18 12:09:03 +0200

    2D array 5 rows and 5 columns

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    array[i, j] = 0;
+            }
+            array[2, 2] = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    Console.Write("{0} ", array[i, j]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
