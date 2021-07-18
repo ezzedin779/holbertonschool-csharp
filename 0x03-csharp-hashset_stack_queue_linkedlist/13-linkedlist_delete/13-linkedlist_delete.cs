c876e714cc3b141d28e48ea4b69ced1061ca51c3
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-19 01:00:05 +0200

    Delete that please ?

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class  LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        List<int> Ls = new List<int>();
+        int count = 0;
+        int l = 0;
+        foreach (var i in myLList)
+        {
+            if (count == index)
+            {
+                count++;
+                continue;
+            }
+            Ls.Add(i);
+            count++;
+        }
+        l = myLList.Count;
+        for (int i = 1; i <= l; i++)
+            myLList.RemoveFirst();
+        foreach (var i in Ls)
+            myLList.AddLast(i);
+    }
+}
