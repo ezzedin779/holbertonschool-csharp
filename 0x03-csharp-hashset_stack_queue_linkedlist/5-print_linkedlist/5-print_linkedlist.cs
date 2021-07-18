3acd534d28e7fdfbc1fe65d92e6d1c77ae8dc1e3
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-19 00:06:34 +0200

    Create and print LList

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class  LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> Ls = new LinkedList<int>();
+        if (size < 0)
+            return (Ls);
+        for (int i = 0; i < size; i++)
+            Ls.AddLast(i);
+        foreach (var i in Ls)
+            Console.WriteLine(i);
+        return (Ls);
+    }
+}
