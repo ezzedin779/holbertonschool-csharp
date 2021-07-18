58734980b86f44a582f387212c4c285ed30351cf
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-19 00:49:51 +0200

    Inserted ?

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class  LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        List<int> Ls = new List<int>();
+        int count = 0;
+        myLList.AddLast(n);
+        foreach (var i in myLList)
+            Ls.Add(i);
+        count = myLList.Count - 1;
+        for (int i = 0; i <= count; i++)
+            myLList.RemoveLast();
+        Ls.Sort();
+        foreach (var i in Ls)
+            myLList.AddLast(i);
+        return (myLList.First);
+    }
+}
