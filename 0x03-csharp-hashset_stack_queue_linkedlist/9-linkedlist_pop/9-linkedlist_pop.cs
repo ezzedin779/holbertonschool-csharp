ca84e7efbb45c83ec061b67e2c15fdd6ae9719e3
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-19 00:25:34 +0200

    Pop first ?

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class  LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        foreach (var i in myLList)
+        {
+            myLList.RemoveFirst();
+            return (i);
+        }
+        return (0);
+    }
+}
