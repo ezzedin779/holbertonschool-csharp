f6252b27c5cb7c9fdb209c06312f82e0ec6dc1e0
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-18 10:57:18 +0200

    Add new key and value

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        if (myDict.ContainsKey(key) == true)
+        {
+            myDict[key] = value;
+            return (myDict);
+        }
+        myDict.Add(key, value);
+        return (myDict);
+    }
+}
