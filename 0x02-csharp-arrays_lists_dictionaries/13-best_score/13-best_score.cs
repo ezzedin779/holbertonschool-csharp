cf9a9c85606a06adf68d42de8eaafe10c6a7f16a
Abderrahmen Babchia <2150@holbertonschool.com>
2021-07-18 11:59:24 +0200

    Best Person With Score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int x = 0;
+        string best = "None";
+        foreach (var i in myList)
+        {
+            if (i.Value > x)
+            {
+                x = i.Value;
+                best = i.Key;
+            }
+        }
+        return (best);
+    }
+}
