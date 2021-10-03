7a010f923d06245ef7ef731b61a84da31fed5b42
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-03 12:54:10 +0200

    Safe way to divide ?

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Int
+    {
+        public static void divide(int a, int b)
+        {
+            int i = 0;
+            try
+            {
+                i = a / b;
+            }
+            catch (DivideByZeroException)
+                {
+                    i = 0;
+                    Console.WriteLine("Cannot divide by zero");
+                }
+            finally
+            {
+                console.WriteLine("{0} / {1} = {2}", a, b, i);
+            }
+        }
+    }
