26cb1d21a12616d3802613956c966875ba544fbb
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-06 11:06:05 +0200

    initialise the zombie ?

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// defines the health only
+    /// </summary>
+    class Zombie
+    {
+        /// public field
+        public int? health {get; set;} = null;
+        
+        /// initialize to 0
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
