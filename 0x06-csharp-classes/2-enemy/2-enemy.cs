0cecef27697133dbfb75a7c8b31c88c0a9d1bbfb
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-06 11:11:29 +0200

    Verify the health value

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Enemy Class :o 
+    /// </summary>
+    class Zombie
+    {
+        /// public field
+        public int? health {get; set;} = null;
+        
+        /// initialize to 0
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// a value for health ?
+        public Zombie(int value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+    }
+}
