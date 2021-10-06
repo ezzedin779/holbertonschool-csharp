051a8c145e97b15c1ccfe80d7e5a04e33d438c04
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-06 11:42:59 +0200

    Public property ?

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Enemy Class :o 
+    /// </summary>
+    class Zombie
+    {
+        /// private health
+        private int health;
+        /// private name
+        private string name = "(No name)";
+        
+        /// property of name (public)
+        public string Name
+        {
+            get {return name;}
+            set { name = value;}
+        }
+
+        /// initialize to 0
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// a value for health ?
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+        
+        /// tell me how much health u have ?
+        public int GetHealth()
+        {
+            return (health);
+        }
+    }
+}
