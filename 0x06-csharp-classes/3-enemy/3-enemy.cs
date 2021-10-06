6293a946f4ce708ec615d5fdbb02098416021b9b
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-06 11:17:13 +0200

    Show me what u've got ?

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Enemy Class :o 
+    /// </summary>
+    class Zombie
+    {
+        /// private field
+        private int health;
+        
+        /// initialize to 0
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// a value for health ?
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+        
+        /// tell me how much health u have ?
+        public int GetHealth()
+        {
+            return (health);
+        }
+    }
+}
