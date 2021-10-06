0520bff079a4a6f6b994f68ec2fedaafdebd5734
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-06 09:23:54 +0200

    Printing ?

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,23 @@
+﻿enum Rating {
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog {
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+    public override string ToString()
+    {
+        return ("Dog Name: " + name + "\n" + "Age: " + age + "\n" +"Owner: " + owner + "\n" +"Rating: " + rating);
+    }
+}
