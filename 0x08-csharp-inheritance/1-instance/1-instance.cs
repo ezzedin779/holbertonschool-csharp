fd5c1ea5594f1e82db8ca62062d036abaee48153
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-08 12:55:41 +0200

    Are u array ?

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary> the class obj </summary>
+    class Obj
+    {
+        /// <summary> Is this and instance of array ? </summary>
+        public static bool IsInstanceOfArray(object obj)
+        {
+            return (typeof(Array).IsInstanceOfType(obj));
+        }
+    }
