9718ff8e00fa6f9f32a5338d8d5bb6f9eb01596d
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-08 12:09:30 +0200

    Are u int ?

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
     /// <summary> the class obj <summary>
     class Obj
     {
-        /// are u int ?
+        /// <summary> Are u int ? <summary>
         public static bool IsOfTypeInt(object obj)
         {
             if (obj is int){
