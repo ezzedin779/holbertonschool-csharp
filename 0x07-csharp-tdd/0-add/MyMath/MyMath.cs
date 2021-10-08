043c2a61021e117039ed09f90efa539233bf3238
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-08 11:43:23 +0200

    fixed ?

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -10,6 +10,9 @@ namespace MyMath
     public class Operations
     {
         /// the Add function
+        /// <param name="a">val a</param>
+        /// <param name="b"> val b</param>
+        /// <returns> a + b = int</returns>
         public static int Add(int a, int b)
         {
             return a + b;
