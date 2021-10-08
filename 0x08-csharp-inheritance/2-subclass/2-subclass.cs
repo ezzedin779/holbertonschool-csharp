5f3a0b8d1668dc0225f9e304df974cd4f7ed5869
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-08 13:01:45 +0200

    Where r u from ?

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// the class obj
+/// </summary>
+    class Obj
+    {
+        /// <summary>
+        /// are u from base type ?
+        /// </summary>
+        /// <param name="derivedType">where are u from</param>
+        /// <param name="baseType">Tunisia</param>
+        /// <returns>bool</returns>
+        public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+        {
+            return (derivedType.IsSubclassOf(baseType));
+        }
+    }
