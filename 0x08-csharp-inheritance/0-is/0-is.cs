dddab11309170f5c301f29cc7db2d69e5fb53ff1
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-08 12:06:42 +0200

    Are u int ?

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+    /// <summary> the class obj <summary>
+    class Obj
+    {
+        /// are u int ?
+        public static bool IsOfTypeInt(object obj)
+        {
+            if (obj is int){
+                return(True);
+            }
+            return (False);
+        }
+    }
