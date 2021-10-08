30bea4707e05feca89aeee775610f92dc2f0dd23
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-08 11:36:52 +0200

    Add Function

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+/// <summary>
+/// Creating an operation class simple add ...
+/// </summary>
+namespace MyMath
+{
+    /// <summary>
+    /// the class of all operations
+    /// </summary>
+    public class Operations
+    {
+        /// the Add function
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
