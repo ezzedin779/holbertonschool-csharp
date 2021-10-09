131eab8f8d3fdc98f53d1362488766f178d036a3
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-09 08:55:18 +0200

    get ur animal class dog

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+    /// <summary>
+        /// The Famous Class Animal
+    /// </summary>
+    class Animal
+    {
+    }
+
+    /// <summary>
+        /// The Dog Class Derived from Animal
+    /// </summary>
+    class Dog : Animal
+    {
+    }
