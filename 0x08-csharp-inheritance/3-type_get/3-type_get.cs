d3f0ae1b827a30003e9d458966f1f3748890c9f7
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-09 08:47:20 +0200

    Gimme ur ID

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+/// <summary>
+/// the class obj 
+/// </summary>
+
+    class obj
+    {
+        /// <summary>
+        /// check who is this myobj
+        /// </summary>
+        /// <param name="myObj">the object to check</param>
+        public static void Print(object myObj)
+        {
+            TypeInfo t = myobj.GetType().GetTypeInfo();
+            IEnumerable<PropertyInfo> pro = t.GetProperties();
+            IEnumerable<MethoInfo> me = t.GetMethods();
+            Console.WriteLine(t.Name + " Properties:");
+            foreach(PropertyInfo i in pro)
+                Console.WriteLine(i.Name);
+            Console.WriteLine(t.Name + " Methods:");
+            foreach (MethodInfo x in me)
+                Console.WriteLine(x.Name);
+        }
+    }
