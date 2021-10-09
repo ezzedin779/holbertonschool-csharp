79714515069693b27b153495c8dd575653f74887
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-09 09:08:58 +0200

    HELLO Advanced Rect

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+    /// <summary>
+        /// The class shape
+    /// </summary>
+    class Shape
+    {
+        /// <summary>
+        /// Throw an exception
+        /// </summary>
+        /// <returns>An exception</returns>
+        public virtual int Area()
+        {
+            throw new NotImplementedException("Area() is not implemented");
+        }
+    }
+
+    /// <summary>
+    /// the child rectangle of shape
+    /// </summary>
+    class Rectangle : Shape
+    {
+        private int width;
+        private int height;
+        
+        /// <summary>
+        /// init the width
+        /// </summary>
+        /// <value>the width if not less then 0</value>
+        public int width
+        {
+            get {return width;}
+            set {
+                    if (value < 0)
+                    {    throw new ArgumentException("Width must be greater than or equal to 0");}
+                    else
+                    {width = value;}
+                }
+        }
+
+        /// <summary>
+        /// init the height
+        /// </summary>
+        /// <value>the height value if nothing goes wrong</value>
+        public int height
+        {
+            get {return height;}
+            set {
+                    if (value < 0)
+                    {    throw new ArgumentException("height must be greater than or equal to 0");}
+                    else
+                    {height = value;}
+                }
+        }
+    }
