32e50ebc17198417adeb1ad2c38f339dbe2381b7
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-09 08:59:26 +0200

    Nice Shape ?

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    /// <summary>
+        /// The class shape
+    /// </summary>
+    class Shape
+    {
+        /// <summary>
+        /// Throw an exception
+        /// </summary>
+        /// <returns>An exception</returns>
+        public virtual int Area()
+        {
+            throw new NotImplementedException("Area() is not implemented");
+        }
+    }
