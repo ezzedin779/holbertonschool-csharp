d36977042a45e9fa1f2460d342dfb8abe1bd7a73
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-14 23:17:24 +0200

    the addition

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -16,12 +16,12 @@
 			double[] ok = {-1};
 			if (vector1.Length != vector2.Length)
 				return (ok);
-			if (vector2 == 2)
+			if (vector2.Length == 2)
 			{
 				double[] r1 = {vector1[0] + vector2[0], vector1[1] + vector2[1]};
 				return (r1);
 			}
-			if (vector2 == 3)
+			if (vector2.Length == 3)
 			{
 				double[] r2 = {vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2]};
 				return (r2);
