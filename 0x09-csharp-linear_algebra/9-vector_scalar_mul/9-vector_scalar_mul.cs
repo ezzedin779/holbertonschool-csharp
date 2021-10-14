f9c6fbe5cebdc8f9cd812d37a31374091a824251
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-14 23:25:20 +0200

    scalar ?

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+	/// <summary>
+	/// the vectormath
+	/// </summary>
+    class VectorMath
+    {
+		/// <summary>
+		/// multiply them
+		/// </summary>
+		/// <param name="vector">the vector</param>
+		/// <param name="scalar">the number</param>
+		/// <returns>new vector</returns>
+        public static double[] Multiply(double[] vector, double scalar)
+        {
+			double[] ok = {-1};
+			if (vector.Length == 2)
+			{
+				double[] r1 = {vector[0] * scalar, vector[1] * scalar};
+				return (r1);
+			}
+			if (vector.Length == 3)
+			{
+				double[] r2 = {vector[0] * scalar, vector[1] * scalar, vector[2] + * scalar};
+				return (r2);
+			}
+			else
+				return (ok);
+		}
+    }
