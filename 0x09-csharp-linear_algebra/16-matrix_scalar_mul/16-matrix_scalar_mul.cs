d33f2687443bc58eaef4a5c1adac078d17f71dec
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-15 06:35:37 +0200

    Matrix * scalar

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+	/// <summary>
+	/// the matrix calculations
+	/// </summary>
+    class MatrixMath
+    {
+		
+        public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+        {
+			int len0 = matrix1.GetLength(0);
+			int len1 = matrix1.GetLength(1);
+			if (len != len1 || len > 3 || len < 2)
+				return (new double[,] {{-1}});
+			double[,] r = new double [len0, len1];
+			for (int i = 0; i < len0; i++)
+				for (int j = 0; j < len1; j++)
+					r[i,j] = matrix1[i,j] * scalar;
+			return (r);
+    	}
+	}
