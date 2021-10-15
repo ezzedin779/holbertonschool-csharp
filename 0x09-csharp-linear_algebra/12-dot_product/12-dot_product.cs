95be1004d1fc06af008021b031338ad353cb93bd
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-15 06:18:01 +0200

    the dot

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+	/// <summary>
+	/// the vectormath
+	/// </summary>
+    class VectorMath
+    {
+		
+        public static double DotProduct(double[] vector1, double[] vector2)
+        {
+			if (vector1.Length == vector2.Length && (vector1.Length == 3 || vector1.Length == 2))
+			{
+				double[] r = new double[vector1.Length];
+				double dot = 0;
+				for (var i = 0; i < vector1.Length; i++)
+					r[i] = vector1[i] * vector2[i];
+				foreach (var hi in r)
+					dot = dot + hi;
+				return (dot);
+			}
+			else
+				return (-1);
+		}
+    }
