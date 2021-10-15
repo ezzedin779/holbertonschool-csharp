21e4681e50e386c3100740ae5ccaf90f580de313
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-15 06:28:20 +0200

    sum of 2 matrix

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -8,14 +8,12 @@
 		
         public static double[,] Add(double[,] matrix1, double[,] matrix2)
         {
-			if (matrix1.Length == matrix2.Length && (matrix1.Length == 3 || matrix1.Length == 2))
-			{
-				double[] r = new double[matrix1.Length];
-				for (var i = 0; i < matrix1.Length; i++)
-					r[i] = matrix1[i] + matrix2[i];
-				return (r);
-			}
-			else
-				return (new double[] {-1});
-		}
+			int len = matrix1.GetLength(0);
+			if (len != matrix1.GetLength || len > 3 || len < 2 || len != matrix2.GetLength)
+				return (new double[,] {{-1}});
+			double[,] r = new double [len, len];
+			for (int i = 0; i < len; i++)
+				for (int j = 0; j < len; j++)
+					r[i,j] = matrix1[i,j] + matrix2[i,j];
+			return (r);
     }
