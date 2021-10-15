3be65034c1fec279a10f33ade02d22986325a51d
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-15 06:37:06 +0200

    Matrix * scalar

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -8,14 +8,14 @@
 		
         public static double[,] MultiplyScalar(double[,] matrix, double scalar)
         {
-			int len0 = matrix1.GetLength(0);
-			int len1 = matrix1.GetLength(1);
+			int len0 = matrix.GetLength(0);
+			int len1 = matrix.GetLength(1);
 			if (len != len1 || len > 3 || len < 2)
 				return (new double[,] {{-1}});
 			double[,] r = new double [len0, len1];
 			for (int i = 0; i < len0; i++)
 				for (int j = 0; j < len1; j++)
-					r[i,j] = matrix1[i,j] * scalar;
+					r[i,j] = matrix[i,j] * scalar;
 			return (r);
     	}
 	}
