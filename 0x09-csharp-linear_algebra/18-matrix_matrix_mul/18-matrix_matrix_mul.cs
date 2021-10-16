71e46763307a83bc2c81e6e310e571a932885cd5
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-16 11:51:40 +0200

    Done ?

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+	/// <summary>
+	/// the matrix calculations
+	/// </summary>
+    class MatrixMath
+    {
+		
+        public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+        {
+			int len = matrix1.GetLength(0);
+			if (len != matrix1.GetLength(1) || len > 3 || len < 2 || len != matrix2.GetLength(0))
+				return (new double[,] {{-1}});
+			double[,] r = new double [len, len];
+			for (int i = 0; i < len; i++)
+				for (int j = 0; j < len; j++)
+					r[i,j] = matrix1[i,j] + matrix2[i,j];
+			return (r);
+    	}
+	}
