2f4c0362506983a84cd0f01c8cc359407b8f8bf0
Abderrahmen Babchia <2150@holbertonschool.com>
2021-12-14 21:11:42 +0100

    the queue class ?

--- 0x0A-csharp-generics/0-queue/queue.cs
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -5,6 +5,10 @@
 /// <typeparam name="T">the type to know</typeparam>
 class Queue<T>
 {
+	/// <summary>
+	/// the class method
+	/// </summary>
+	/// <returns>the type to know</returns>
 	public Type CheckType()
 	{
 		return(typeof(T));
