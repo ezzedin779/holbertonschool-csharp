71403aa53d1b8db5085d328fc9debca4f9f7a1f7
Abderrahmen Babchia <2150@holbertonschool.com>
2021-12-14 21:39:46 +0100

    Advanced ?

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+/// <summary>
+/// Generic Class ?
+/// </summary>
+/// <typeparam name="T">the type to know</typeparam>
+class Queue<T>
+{
+	Node head {get; set;}
+	Node tail {get; set;}
+	int count {get; set;}
+	/// <summary>
+	/// the class method
+	/// </summary>
+	/// <returns>the type to know</returns>
+	public Type CheckType()
+	{
+		return(typeof(T));
+	}
+	///<summary> enqueue </summary>
+	public void Enqueue(T value) {
+		if (head == null)
+		{
+			Node newH = new Node(value);
+			head = tail = newH;
+		}
+		else{
+			Node newT = new Node(value);
+			tail.next = newT;
+			tail = newT;
+		}
+		count++;
+	}
+	/// <summary> counter </summary>
+	public int Count(){
+		return count;
+	}
+	/// <summary> Node CLASS </summary>
+	public class Node{
+		public T value {get; set;}
+		public Node next {get; set;}
+
+		///<summary> the value setter ? </summary>
+		public Node(T value)
+		{
+			this.value = value;
+		}
+	}
+}
