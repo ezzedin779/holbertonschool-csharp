04e1615677bc12834038a42a610fa8c34bcaba1a
Abderrahmen Babchia <2150@holbertonschool.com>
2022-01-03 09:56:52 +0100

    Abstract class ?

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+	/// <summary>
+	/// Base class
+	/// </summary>
+    abstract class Base
+    {
+		/// <summary>
+		/// public property
+		/// </summary>
+        public string name {get; set;}
+		/// <summary>
+		/// override the string
+		/// </summary>
+		public override string ToString()
+		{
+			return $"{name} is a {this.GetType()}";
+		}
+    }
