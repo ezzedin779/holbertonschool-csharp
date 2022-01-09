ced7c9a57a6323239f41597fb2b2fb276f0279e9
Abderrahmen Babchia <2150@holbertonschool.com>
2022-01-09 11:37:06 +0100

    the inventory ?

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/Inventory.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Inventory : BaseClass
+{
+	public int quantity = 1;
+	public Item item_id {get; set;}
+	public User user_id {get; set;}
+}
